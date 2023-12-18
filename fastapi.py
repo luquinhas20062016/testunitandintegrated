75f1d42d5028fd7fd173a33686b0b352a2512884
luquinhas20062016 <146724894+luquinhas20062016@users.noreply.github.com>
2023-12-18 20:07:08 -0300

    Add files via upload

---
+++ fastapi.py
@@ -0,0 +1,37 @@
+import unittest
+
+class Calculator:
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self, x, y):
+        if y == 0:
+            raise ValueError("Cannot divide by zero!")
+        return x / y
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calculator = Calculator()
+
+    def test_add(self):
+        self.assertEqual(self.calculator.add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(self.calculator.subtract(5, 3), 2)
+
+    def test_multiply(self):
+        self.assertEqual(self.calculator.multiply(2, 3), 6)
+
+    def test_divide(self):
+        self.assertEqual(self.calculator.divide(6, 3), 2)
+        with self.assertRaises(ValueError):
+            self.calculator.divide(6, 0)
+
+if __name__ == '__main__':
+    unittest.main()
